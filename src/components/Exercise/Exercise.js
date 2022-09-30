f139e2dc357748ef4cda7fd7cc3a8561a0de5e01
ahm-rubayed <rubayedahmed04@gmail.com>
2022-09-30 11:12:06 +0600

    exercise time added

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Exercise = () => {
     return (
-        <div>
+        <div className='mt-5'>
             <h4>Exercise Details</h4>
-            <div className='d-flex bg-white justify-content-between align-items-center my-4 p-4'>
+            <div className='d-flex bg-white justify-content-between align-items-center my-4 rounded py-2 px-4'>
                 <h6>Exercise Time</h6>
                 <p className='ms-4'>200seconds</p>
             </div>
 
-            <div className='d-flex bg-white justify-content-between align-items-center p-4'>
+            <div className='d-flex bg-white justify-content-between align-items-center rounded py-2 px-4'>
                 <h6>Exercise Time</h6>
                 <p>200seconds</p>
             </div>
