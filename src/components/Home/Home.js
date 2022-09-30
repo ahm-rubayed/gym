7e4ac75ea2bbe3d54f5051c3e40b1b4f89744724
ahm-rubayed <rubayedahmed04@gmail.com>
2022-09-30 09:53:58 +0600

    added a file in git ignore

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,15 +11,15 @@ const Home = () => {
     }, [])
 
   return (
-    <div className="home row g-4 ps-5">
+    <div className="row g-4 ps-5 mb-5">
     <h4 className="mt-5">Select Todays exercise</h4>
 
-        <div className="col-8 gym-info row row-cols-1 row-cols-md-3 g-4 mt-4">
+        <div className="col-8 row row-cols-1 row-cols-md-3 g-4 mt-4">
             {
                  items.map(item => <GymItem item ={item}></GymItem>)
             }
         </div>
-        <div className="gym-details col">
+        <div className="col">
             <p>lorem10alskdffff;</p>
         </div>
     </div>
